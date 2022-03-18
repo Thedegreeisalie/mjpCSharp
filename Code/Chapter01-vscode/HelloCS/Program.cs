86a1a1c2c005a57956ddb0eb98d044a6f17a30d5
Moore <moore.jeremiah.mark@gmail.com>
2022-03-18 12:38:56 -0600

    Chapter 1

---
+++ Code/Chapter01-vscode/HelloCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! this is an application writtien in c# and making me want to cry");
+        }
+    }
+}
