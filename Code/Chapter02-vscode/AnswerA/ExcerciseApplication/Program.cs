aabc8d0cc3d7354e047d65e70f0f226af02dfd49
Moore <moore.jeremiah.mark@gmail.com>
2022-03-23 10:27:11 -0600

    Chapter 3

---
+++ Code/Chapter02-vscode/AnswerA/ExcerciseApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExcerciseApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int max = 500;
+            for (byte i = 0; i < max; i++)
+            {
+                Console.Write(" {0} ", i);
+            }
+        }
+    }
+}
